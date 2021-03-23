45456f33034ab1893ee692b54f64490632bcf796
Шумилин Иван <mirovengil@gmail.com>
2021-03-23 14:24:46 +0300

    В скрипте смены цветовой палитры устранена ещё одна бага; работоспособность проверена =)

--- css/color_changer.py
+++ css/color_changer.py
@@ -13,19 +13,23 @@ fake_letter_color = "00a8f5"
 #Фона сайта
 main_color = "bcfab6"
 
+#Плашки в верху сайта
+header_color = "faab00"
+
 def replace_colors_to_values(string):
-    string.replace("nav_color", "#" + nav_color)
-    string.replace("readdr_color", "#" + readdr_color)
-    string.replace("readdr_hover_color", "#" + readdr_hover_color)
-    string.replace("fake_letter_color", "#" + fake_letter_color)
-    string.replace("main_color", "#" + main_color)
+    string = string.replace("nav_color", "#" + nav_color)
+    string = string.replace("readdr_color", "#" + readdr_color)
+    string = string.replace("readdr_hover_color", "#" + readdr_hover_color)
+    string = string.replace("fake_letter_color", "#" + fake_letter_color)
+    string = string.replace("main_color", "#" + main_color)
+    string = string.replace("header_color", "#" + header_color)
     return string
 
 stdin = open("./css/style_template_for_experiment.txt", 'r')
 stdout = open("./css/style.css", 'w')
 
 for i in stdin:
-    print(replace_colors_to_values(i), file = stdout)
+    print(replace_colors_to_values(i), file = stdout, end = "")
 
 stdin.close()
 stdout.close()
