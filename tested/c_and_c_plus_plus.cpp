a12ac440455b43537d11dd3efb08c04362e36870
Шумилин Иван <mirovengil@gmail.com>
2021-04-15 18:02:31 +0300

    Добавлена страница stack.

--- tested/c_and_c_plus_plus.cpp
+++ tested/c_and_c_plus_plus.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include <vector>
 
+void clear(int &length)
+{
+    length = 0;
+};
+
+void add(int *mass, int &length, int value)
+{
+    mass[length] = value;
+    length += 1;
+};
+
+int get(int *mass, int length)
+{
+    if (length == 0)
+        exit(1); //Аварийный выход из функции
+    return mass[length - 1];
+}; 
+
+void del(int &length)
+{
+    length -= 1;
+    if (length < 0)
+        length = 0;
+};
+
 int main(void)
 {
-    int n;
-    std::cin >> n;
-    int mass[n];
-    for (int i = 0; i < n; ++i)
-        std::cin >> mass[i];
-    
-    for (int i = 0; i < n / 2; ++i)
+    int mass[100];
+    int length;
+    clear(length);
+    add(mass, length, 1);
+    add(mass, length, 2);
+    add(mass, length, 3);
+    for (int i = 0; i < 3; ++i)
     {
-        int temp = mass[i];
-        mass[i] = mass[n - 1 - i];
-        mass[n - 1 - i] = temp;
+        std::cout << get(mass, length) << " ";
+        del(length);
     };
-    
-    for (int i = 0; i < n; ++i)
-        std::cout << mass[i] << " ";
     return 0;
 };
